2bf572c2bf0c9e2f53bb5b3caee69c5719d84f91
zhimmat <zeyad.himmat@live.com>
2016-02-26 20:15:22 -0500

    added index and js code

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var aColour;
+
+var aCreature;
+
+var anAdjective;
+
+var aPast;
+
+var aStory;
+
+var aColour = prompt('A colour (orange, indigo, etc.)');
+
+var aCreature = prompt('A creature (dragon, moth, etc.)');
