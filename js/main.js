7462f0c87a315c6b1fdc463cb118d5a0ed2b1ce2
zhimmat <zeyad.himmat@live.com>
2016-02-26 20:15:54 -0500

    final js code in main.js

--- js/main.js
+++ js/main.js
@@ -17,3 +17,17 @@ var anAdjective = prompt('An adjective (beautiful, super, etc.)');
 var aPast = prompt('A past tense verb (ran, burped, etc.)');
 
 var aStory = prompt('Which story would you like?\n a) Snacking\n b) Escaping\n c) Winning');
+
+alert("Let's make a story together!");
+
+if (aStory == 'a') {
+  document.write('After snacking on '  + anAdjective + ' treats, the ' + aColour + ' bellied ' + aCreature + ' ' + aPast + ' for hours.');
+}
+
+if (aStory == 'b') {
+  document.write('Ameilia ' + aPast + ' through the ' + anAdjective + ' ' + aColour + ' nebula escaping the space ' + aCreature + '.');
+}
+
+if (aStory == 'c') {
+  document.write('Jackson chose his ' + anAdjective + ',' + aColour + ' ' + aCreature + ' card and ' + aPast + ' it to the table knowing he won.');
+}
