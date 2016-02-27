9ae3bba7e4649e1fcc909b3b130b677fed1a54da
zhimmat <zeyad.himmat@live.com>
2016-02-26 20:15:40 -0500

    more js code

--- js/main.js
+++ js/main.js
@@ -11,3 +11,9 @@ var aStory;
 var aColour = prompt('A colour (orange, indigo, etc.)');
 
 var aCreature = prompt('A creature (dragon, moth, etc.)');
+
+var anAdjective = prompt('An adjective (beautiful, super, etc.)');
+
+var aPast = prompt('A past tense verb (ran, burped, etc.)');
+
+var aStory = prompt('Which story would you like?\n a) Snacking\n b) Escaping\n c) Winning');
